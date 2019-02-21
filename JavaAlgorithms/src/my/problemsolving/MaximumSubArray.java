f89d12b439331f52d4ab8844dabe489adf7bed40
Karthik <pk.preethikarthik@gmail.com>
2019-02-21 22:30:45 +0530

    Better naming

--- JavaAlgorithms/src/my/problemsolving/MaximumSubArray.java
+++ JavaAlgorithms/src/my/problemsolving/MaximumSubArray.java
@@ -8,20 +8,20 @@ public class MaximumSubArray {
 	}
 
 	static int maxSubArraySum(int a[]) {
-		int max_so_far = Integer.MIN_VALUE;
-		int max_ending_here = 0;
+		int maxSum = Integer.MIN_VALUE;
+		int sumTillNow = 0;
 
 		for (int i = 0; i < a.length; i++) {
-			max_ending_here = max_ending_here + a[i];
-			System.out.println("i = " + i + ", max_ending_here = " + max_ending_here);
-			if (max_ending_here < 0) {
-				max_ending_here = 0;
+			sumTillNow += a[i];
+			System.out.println("i = " + i + ", sumTillNow = " + sumTillNow);
+			if (sumTillNow < 0) {
+				sumTillNow = 0;
 			}
-			if (max_so_far < max_ending_here) {
-				max_so_far = max_ending_here;
+			if (sumTillNow > maxSum) {
+				maxSum = sumTillNow;
 			}
 		}
-		return max_so_far;
+		return maxSum;
 	}
 
 }
